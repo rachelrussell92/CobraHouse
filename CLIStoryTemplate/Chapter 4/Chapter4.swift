19e7ad8e6507f1b04c060f809b9c7beb173f8045
leon Singleton <Killtheking727@gmail.com>
2023-10-19 10:19:51 -0400

    thrusday

--- CLIStoryTemplate/Chapter 4/Chapter4.swift
+++ CLIStoryTemplate/Chapter 4/Chapter4.swift
@@ -5,6 +5,25 @@
 
 import Foundation
 
+func seenAMonster(){
+    print("Run as Fast as they can until they escape away from the monsters")
+}
 func chapterFour() {
-    // Your portion of the story goes here
+    func actOne(){  print( "On a mission to find the cure \(theGirl), \(theFriends) ran frantical look for book on what happen on \(theIsland).")
+        print( "The went back to \(theTown) looking for any one or any book to help them.")
+        print( "They stump across an old lady that told them a story about \(theIsland)")
+        print( "She told the that after the People of \(theTown) getting the island from the indians they didnt fully pay them but wouldnt give the land back. ")
+        print( "So they put a curse on the island and snakes from near and wide convegred on the island which made it inhabitable ")
+        print( " The natives of \(theTown) in there fury tried to do everything to clear the snake.")
+        print("It only made the snake more ferious which cause them to move from the island and into \(theTown), the snakes started to attack the people and kill them.")
+        print("The people of \(theTown) finally had enough so they beg the cheif to make them go away.")
+        print("Once they repaid the indians the Chief fianlly told them they need to go to the three parts of \(theTown) and find and collect the three magical pigs.")
+        print("Then slide on all the tribe leader and collect part of the River Diamond ")
+    }
+    func actTwo(){
+        
+        
+    }
+   
+    
 }
