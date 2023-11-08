c7dd384237f67e78f781b2ebfa333b8da51365e9
Shadonna Bradford <shadonna.bradford@gmail.com>
2023-11-08 06:15:13 -0800

    new one

--- CLIStoryTemplate/Chapter 3/Chapter3.swift
+++ CLIStoryTemplate/Chapter 3/Chapter3.swift
@@ -6,16 +6,16 @@
 import Foundation
 var AR = "Aunt Rachel"
 var US = "Uncle Shad"
-let ttH = theGirl
+let theGirl = "Thea"
 struct people{
     var name:String = ""
     var relationship: String = ""
     var age:Int = 0
 }
-var family:[people] = [people(name: "thea", relationship: "self", age: 15), people(name: "rachael", relationship: "Aunt", age: 45), people(name: "shad", relationship: "uncle", age: 43)]
+var fam:[people] = [people(name: "thea", relationship: "self", age: 15), people(name: "rachael", relationship: "Aunt", age: 45), people(name: "shad", relationship: "uncle", age: 43)]
 var ttHhasazombiespelluponher:Bool = true
 var ttHdoesnothaveazombiespelluponher:Bool = false
-var characterName = ["Aunt Rachael", "Uncle Shad", theFriends]
+var characterName = ["Aunt Rachael", "Uncle Shad", "theFriends"]
 func knock() -> String {
     return "knocking on the door"
     
@@ -29,7 +29,7 @@ func enterDoor() -> Bool{
 
 func chapterThree() {
     // Your portion of the story goes here
-    print("'\nAre you sure?' \(family[0]) says. \n'Positive' \(family[2]) says. \(family) looks at Thea in disbelief and they stare at each other for what seems like minutes but was actually a few seconds. Suddenly, there was a knocking noise that seemed to be coming from a door. \(theGirl) looks away and then looks back at her Uncle Shad who turned to her with a mysterious glare. After \(theGirl) convinces her aunt & uncle that she saw nothing in the house, she excuses herself from the table and goes to her room. She plops down on the bed with irritation because she wasn't supposed to be caught going to the house. As \(theGirl) lay in bed thinking, she remembered the knocking noise from the door. She wondered what it could have been that made the noise and why Uncle Shad looked at her mysteriously. Deep in thought, \(theGirl) started to wander around her room and when she opened her drawer to put on her nightcap there was mysteriously a picture of her parents that she had been looking for, for a very long time. \(theGirl) looked at the picture confused wondering how it suddenly appeared in this drawer after all this time. \(theGirl) did enough thinking and decided to call it a night. \(theGirl) could hardly get any sleep that night and she woke up in the middle of the night from a dream that reminded her of the fun times she had with her parents, specifically when they took her to the county state fair. The one thing that made her happy was she saw the cobra snake stuffed animal that her dad won for her. When \(theGirl) woke up the next morning she was still thinking about the picture of her parents that suddenly appeared in her drawer. She found it funny how that same night she also dreamed about her parents. The picture, the dream, and the knock led her back into detective mode and due to her aunt & uncles reaction to her being at the house she decided to not tell them.")
+    print("'\nAre you sure?' \(family[0]) says. \n'Positive' \(fam[2]) says. \(fam) looks at Thea in disbelief and they stare at each other for what seems like minutes but was actually a few seconds. Suddenly, there was a knocking noise that seemed to be coming from a door. \(theGirl) looks away and then looks back at her Uncle Shad who turned to her with a mysterious glare. After \(theGirl) convinces her aunt & uncle that she saw nothing in the house, she excuses herself from the table and goes to her room. She plops down on the bed with irritation because she wasn't supposed to be caught going to the house. As \(theGirl) lay in bed thinking, she remembered the knocking noise from the door. She wondered what it could have been that made the noise and why Uncle Shad looked at her mysteriously. Deep in thought, \(theGirl) started to wander around her room and when she opened her drawer to put on her nightcap there was mysteriously a picture of her parents that she had been looking for, for a very long time. \(theGirl) looked at the picture confused wondering how it suddenly appeared in this drawer after all this time. \(theGirl) did enough thinking and decided to call it a night. \(theGirl) could hardly get any sleep that night and she woke up in the middle of the night from a dream that reminded her of the fun times she had with her parents, specifically when they took her to the county state fair. The one thing that made her happy was she saw the cobra snake stuffed animal that her dad won for her. When \(theGirl) woke up the next morning she was still thinking about the picture of her parents that suddenly appeared in her drawer. She found it funny how that same night she also dreamed about her parents. The picture, the dream, and the knock led her back into detective mode and due to her aunt & uncles reaction to her being at the house she decided to not tell them.")
     
     for i in 2...4{
         print("\nThea left home and went back to Cobra House.")
